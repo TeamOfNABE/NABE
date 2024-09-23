e9a8dc73dea06ae413775b042d6917929be8a2b0
ln32 <jhchoi602@gmail.com>
2024-09-23 14:13:33 +0900

    Source TidyUP

--- Assets/Association/JH/Scripts/ManagingData/DataManager.cs
+++ Assets/Association/JH/Scripts/ManagingData/DataManager.cs
@@ -6,6 +6,6 @@ public class DataManager : MonoSingleton<DataManager>
     [SerializeField] internal ManagingCurrencyInt _CurrencyDBG = new();
     [SerializeField] internal ManagingVisualTypeString _VisualDataDBG = new();
 
-    public ManagingCurrencyInt Currency { get { return _CurrencyDBG; } }
-    public ManagingVisualTypeString VisualData { get { return _VisualDataDBG; } }
+    public IManagingDataHandler<CurrencyType_Int,int> Currency { get { return _CurrencyDBG; } }
+    public IManagingDataHandler<VisualType_String, string> VisualData { get { return _VisualDataDBG; } }
 }
